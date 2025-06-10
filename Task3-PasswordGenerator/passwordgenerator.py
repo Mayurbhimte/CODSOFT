1b0904065354d4e2aff74a5a76b0c4d63a23d5ec
Mayurbhimte <mayurbhimte30@gmail.com>
2025-06-10 23:32:28 +0530

    Added Task3-PasswordGenerator

---
+++ Task3-PasswordGenerator/passwordgenerator.py
@@ -0,0 +1,23 @@
+# Task 3 - Password Generator
+
+import random
+import string
+
+def generate_password(length):
+    if length < 4:
+        return "Password length should be at least 4 characters."
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("Password Generator")
+    try:
+        length = int(input("Enter the desired password length: "))
+        password = generate_password(length)
+        print(f"Generated Password: {password}")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+main()
